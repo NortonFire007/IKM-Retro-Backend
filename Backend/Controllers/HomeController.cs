2164f0b553ba3d5532dc63592617ddd0e0149a3a
Uaplexer <rupleflex31@gmail.com>
2025-03-31 20:24:07 +0300

    First commit

---
+++ Backend/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKM_Retro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
---
+++ Backend/Data/RetroDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IKM_Retro.Data
+{
+    public class RetroDbContext(DbContextOptions<RetroDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
