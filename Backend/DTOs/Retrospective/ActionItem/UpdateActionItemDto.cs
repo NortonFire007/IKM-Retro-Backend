9ec7145602f0c5b213ffff3d846f468ef4e7c58b
NortonFire007 <rioknuto@gmail.com>
2025-05-05 14:08:38 +0300

    Added UpdateActionItemDto

---
+++ Backend/DTOs/Retrospective/ActionItem/UpdateActionItemDto.cs
@@ -0,0 +1,12 @@
+﻿using IKM_Retro.Enums;
+
+namespace IKM_Retro.DTOs.Retrospective.ActionItem;
+
+public class UpdateActionItemDto
+{
+    public string? Description { get; set; }
+    public ActionItemPriority? Priority { get; set; }
+    public ActionItemStatus? Status { get; set; }
+    public DateTime? DueDate { get; set; }
+    public Guid? AssignedUserId { get; set; }
+}
